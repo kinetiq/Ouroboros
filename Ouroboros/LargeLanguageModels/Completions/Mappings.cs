9803ccf34d61cde6429a700b8af8f1a3e95aa09f
Brian MacKay <kinetiq@users.noreply.github.com>
2025-04-23 09:25:26 -0700

    Hard-coded gpt 4.1 support.

--- Ouroboros/LargeLanguageModels/Completions/Mappings.cs
+++ Ouroboros/LargeLanguageModels/Completions/Mappings.cs
@@ -39,6 +39,12 @@ internal class Mappings
     /// </summary>
     internal static string GetModelNameAsString(OuroModels? ouroModel)
     {
+        // Models not yet explicitly supported by Betalgo.
+        if (ouroModel == OuroModels.Gpt_4_1)
+        {
+            return "gpt-4.1"; 
+        }
+
         var betalgoModel = ouroModel switch
         {
             null => DefaultBetalgoModel,
@@ -55,7 +61,7 @@ internal class Mappings
             OuroModels.Gpt3_5_Turbo => Models.Model.Gpt_3_5_Turbo,
             OuroModels.Gpt3_5_Turbo_16k => Models.Model.Gpt_3_5_Turbo_16k,
             OuroModels.Gpt_4 => Models.Model.Gpt_4,
-            OuroModels.Gpt_4_1 => Models.Model.Gpt_4o, // TODO: THIS IS WRONG 4_1 does not exist yet in Betalgo!!
+            OuroModels.Gpt_4_1 => throw new NotSupportedException("Replace with Betalgo model when available."),
             OuroModels.Gpt_4o => Models.Model.Gpt_4o,
             OuroModels.Gpt_4_32k => Models.Model.Gpt_4_32k,
             
