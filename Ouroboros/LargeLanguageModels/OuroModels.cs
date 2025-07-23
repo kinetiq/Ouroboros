4cdf9ea2f8b892ac6d940656cd29f2bcadfa6acc
Brian MacKay <kinetiq@users.noreply.github.com>
2025-07-23 11:28:40 -0700

    Package upgrades and fixes for o3, o3-mini, and o4-mini.

--- Ouroboros/LargeLanguageModels/OuroModels.cs
+++ Ouroboros/LargeLanguageModels/OuroModels.cs
@@ -23,9 +23,9 @@ public enum OuroModels
     [MaxTokens(128000)]  [Chat]     Gpt_4_turbo,
     [MaxTokens(128000)]  [Chat]     Gpt_4_turbo_2024_04_09,
     [MaxTokens(32768)]   [Chat]     Gpt_4_32k,
-    [MaxTokens(100000)]  [Chat]     Gpt_o3,
-    [MaxTokens(100000)]  [Chat]     Gpt_o3_mini,
-    [MaxTokens(100000)]  [Chat]     Gpt_o4_mini
+    [MaxTokens(100000)]  [Chat]     o3,
+    [MaxTokens(100000)]  [Chat]     o3_mini,
+    [MaxTokens(100000)]  [Chat]     o4_mini
 }
 
 class MaxTokensAttribute : Attribute
