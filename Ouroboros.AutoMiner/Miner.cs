2e81aa3a3a2854e0490156aea29a551e70c83065
Brian MacKay <kinetiq@users.noreply.github.com>
2026-01-28 15:37:08 -0800

    Refactor Miner to use primary constructor and update dialog handling methods

--- Ouroboros.AutoMiner/Miner.cs
+++ Ouroboros.AutoMiner/Miner.cs
@@ -4,10 +4,8 @@ using Ouroboros.Extensions;
 
 namespace Ouroboros.AutoMiner;
 
-public class Miner
+public class Miner(OuroClient ouroClient)
 {
-    private readonly OuroClient OuroClient;
-
     public async Task<List<string>> MineAsync(string rawData, int insightGoal = 1, int maxAttempts = 10)
     {
         var results = new List<string>();
@@ -26,10 +24,10 @@ public class Miner
             if (likert < LikertAgreement4.Agree)
                 continue;
 
-            var insight = doc.GetByName("insight");
+            var insight = doc.Variables["insight"];
             results.Add(insight);
 
-            var citation = doc.GetByName("citations");
+            var citation = doc.Variables["citations"];
             results.Add("citations:");
             results.Add(citation);
 
@@ -37,9 +35,6 @@ public class Miner
         } while (insights < insightGoal && attempts < maxAttempts);
 
         // TODO: go further - can we propose ways to research this? Sources to explore? 
-        // TODO: maybe we can say that after researching it against all human knowledge, I discovered... 
-        
-        // TODO: maybe build a genius researcher. Maybe we could stage a debate between some of the best minds in history on the topic.
 
         return results;
     }
@@ -49,25 +44,23 @@ public class Miner
     /// </summary>
     private async Task<Dialog> GenerateInsightAsync(string text)
     {
-        var dialog = OuroClient.CreateDialog();
+        var dialog = ouroClient.CreateDialog();
 
        var response = await dialog.SystemMessage(text)
             .UserMessage("[INSIGHT] Based on this data, what is a clever insight that is worthy of further research?")
-            .SendAndAppend("insight")
+            .SendAndAppend()
+            .StoreOutputAs("insight")
             .UserMessage("[CITATIONS] Using the data above, provide a few quotes that best support or prove the insight.")
-            .SendAndAppend("citations")
+            .SendAndAppend()
+            .StoreOutputAs("citations")
             .UserMessage("[VALIDATION] Do you agree that these quotes exist in the data and sufficiently justify this insight? To qualify, the quotes must clearly exist in the data above. Answer using only these words: Strongly Disagree, Disagree, Agree, Strongly Agree")
-            .SendAndAppend("validation")
+            .SendAndAppend()
+            .StoreOutputAs("validation")
             .Execute();
 
         if (!response.Success)
             throw new InvalidOperationException("Failed: " + response.ResponseText);
 
         return dialog;
-    }
-
-    public Miner(OuroClient ouroClient)
-    {
-        OuroClient = ouroClient;
     }
 }
