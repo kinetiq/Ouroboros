8acd16252dfec3ed85095144a45194180a6c5f68
Brian MacKay <kinetiq@users.noreply.github.com>
2026-01-30 08:01:29 -0800

    Update default chat model to Gpt_5_mini and add reasoning effort option

--- Ouroboros/Constants.cs
+++ Ouroboros/Constants.cs
@@ -4,10 +4,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Betalgo.Ranul.OpenAI.Contracts.Enums;
 
 namespace Ouroboros;
 public class Constants
 {
     public const OuroModels DefaultCompletionModel = OuroModels.TextDavinciV3;
-    public const OuroModels DefaultChatModel = OuroModels.Gpt_4_1;
+    public const OuroModels DefaultChatModel = OuroModels.Gpt_5_mini;
+
+
+    /// <summary>
+    /// Reasoning effort to use when running in default chat mode; this only takes effect
+    /// when the default chat model is being used.
+    /// </summary>
+    public static readonly ReasoningEffort? DefaultReasoningEffort = null;
 }
